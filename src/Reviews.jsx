6c9335de66a818b4316c164079c7a49a75e64852
Hameedah <hameedahadeyemi1@gmail.com>
2026-04-20 00:37:24 +0100

    Fix casing of App.css import for consistency

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -5,7 +5,7 @@ import { Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import "./app.css";
+import "./App.css";
 
 import user1 from "./assets/user1.jpg";
 import user2 from "./assets/user2.jpg";
